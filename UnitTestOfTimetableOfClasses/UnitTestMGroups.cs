e59fc5bc191f10179645cc9d5d32bf8551ee6b8f
TanyaMalis <tanya.malishev@gmail.com>
2018-06-27 14:48:58 +0300

    Тест для MGroups

---
+++ UnitTestOfTimetableOfClasses/UnitTestMGroups.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimetableOfClasses;
+using System.Windows.Forms;
+using LibOfTimetableOfClasses;
+
+namespace UnitTestOfTimetableOfClasses
+{
+    [TestClass]
+    class UnitTestMGroups
+    {
+        private static MGroup G;
+        [TestInitialize]
+        public void ClassInitialize()
+        {
+            G = new MGroup();
+            G.Сipher = "";
+            G.Population = 0;
+
+        }
+        [TestMethod]
+        public void TestMethodID()
+        {
+         
+            Assert.AreEqual(typeof(Guid), G.Id.GetType(), "Тип поля ID не совпадает с диаграммой");
+        }
+        
+        public void TestMethodCipher()
+        {
+            Assert.AreEqual(typeof(String), G.Сipher.GetType(), "Тип поля Cipher не совпадает с диаграммой");
+        }
+        
+        public void TestMethodPopulation()
+        {
+            Assert.AreEqual(typeof(Byte), G.Population.GetType(), "Тип поля Population не совпадает с диаграммой");
+        }
+    }
+}
