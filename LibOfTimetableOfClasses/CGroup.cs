ed0b395d7c6f7012bd6ed5e03707a720658cb828
blinker44 <blinker4@yandex.ru>
2018-10-19 18:43:14 +0300

    Метод Add(group:Mgroup):bool реализован

--- LibOfTimetableOfClasses/CGroup.cs
+++ LibOfTimetableOfClasses/CGroup.cs
@@ -8,5 +8,31 @@ namespace LibOfTimetableOfClasses
 {
     public class CGroup
     {
+        List<MGroup> grouplist = new List<MGroup>();
+
+        /// <summary>
+        /// Добавляет группу в grouplist
+        /// </summary>
+        /// <param name="group">Добавляемая группа</param>
+        /// <returns>true => если группа успешно добавлена, false => если добавляемая группа уже существует</returns>
+        public bool Add(MGroup group)
+        {
+            int count = 0;
+
+            foreach(MGroup m in grouplist)
+            {
+                count++;
+                if (m.Id == group.Id)
+                    break;
+            }
+
+            if (count == grouplist.Count)
+            {
+                grouplist.Add(group);
+                return true;
+            }
+            else
+                return false;
+        }
     }
 }
